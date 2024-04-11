a1478b98b1b59ab138a17ae2fa1c755c302c748e
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-11 23:39:02 +0200

    task-6 done

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,37 @@
+const input = document.querySelector('input');
+const dataCreateBtn = document.querySelector('button[data-create]');
+const dataDestroyBtn = document.querySelector('button[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+
+dataCreateBtn.addEventListener('click', OnCreateClick);
+dataDestroyBtn.addEventListener('click', OnDestroyClick);
+
+function OnCreateClick() {
+  let amount = input.value;
+  if (amount < 1 || amount > 100) {
+    return alert('Please enter the correct amount between 1 and 100');
+  }
+  OnDestroyClick();
+  createBoxes(amount);
+}
+
+function OnDestroyClick() {
+  divBoxes.innerHTML = '';
+  input.value = '';
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    let box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    divBoxes.append(box);
+    size += 10;
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
